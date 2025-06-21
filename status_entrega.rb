b697ba346402eb36dac0f76c6ba5c4ec7235e432
IagoRochaDev <iagor.oliveira00@gmail.com>
2025-06-21 12:05:41 -0300

    add atributo peso_carga a classe StatusEntrega

--- status_entrega.rb
+++ status_entrega.rb
@@ -1,11 +1,12 @@
 # status_entrega.rb
 class StatusEntrega
-  attr_accessor :clima, :estrada, :zona, :distancia
+  attr_accessor :clima, :estrada, :zona, :distancia, :pose_carga
 
-  def initialize(clima="sol", estrada="asfalto", zona="urbana", distancia=0)
+  def initialize(clima="sol", estrada="asfalto", zona="urbana", distancia=0, peso_carga = 0)
     @clima = clima
     @estrada = estrada
     @zona = zona
     @distancia = distancia
+    @peso_carga = peso_carga
   end
 end
