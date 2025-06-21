feb6aec685fdee34a01ab0b077b7e273e753469e
IagoRochaDev <iagor.oliveira00@gmail.com>
2025-06-21 12:17:00 -0300

    Modificada a menagem de status

--- verificador_entrega.rb
+++ verificador_entrega.rb
@@ -7,11 +7,12 @@ class VerificadorEntregas
           tempo: meio.tempo(status_entrega),
           custo: meio.valor_viagem(status_entrega)
           carga_max: meio.carga_maxima(status_entrega)
+          status: "Disponível o veiculo!!"
         }
       else
         {
           meio: meio.class.name,
-          status: "Indisponível no clima atual"
+          status: "Indisponível o veiculo!!"
         }
       end
     end
