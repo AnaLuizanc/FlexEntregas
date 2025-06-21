ee7abba94d8b6f85fa28f303d134fe920bf51e41
IagoRochaDev <iagor.oliveira00@gmail.com>
2025-06-21 11:54:07 -0300

    Add atributo distancia

--- status_entrega.rb
+++ status_entrega.rb
@@ -1,10 +1,11 @@
 # status_entrega.rb
 class StatusEntrega
-  attr_accessor :clima, :estrada, :zona
+  attr_accessor :clima, :estrada, :zona, :distancia
 
-  def initialize(clima="sol", estrada="asfalto", zona="urbana")
+  def initialize(clima="sol", estrada="asfalto", zona="urbana", distancia=0)
     @clima = clima
     @estrada = estrada
     @zona = zona
+    @distancia = distancia
   end
 end
